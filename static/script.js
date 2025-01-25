1af305d8c65801bf6e2eaaa43884fe84c7341d4f
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-01-25 14:26:00 -0300

    finnaly reponsive mobile commit

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// Função para alternar a exibição do menu
+function toggleMenu() {
+    const menu = document.getElementById('menuOptions');
+    menu.classList.toggle('active'); // Alterna a classe 'active' para mostrar ou esconder o menu
+
+}
